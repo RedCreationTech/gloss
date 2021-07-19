ed71c3fec835f416a781e5be602b4a8968505a1c
Keivn <kevin.li@redcreation.net>
2021-07-20 00:31:30 +0800

    bump version to 0.2.8

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject org.clojars.zhaoyul/gloss "0.2.7"
+(defproject org.clojars.zhaoyul/gloss "0.2.8"
   :description "speaks in bytes, so that you don't have to"
   :license {:name "Eclipse Public License - v 1.0"
             :url "http://www.eclipse.org/legal/epl-v10.html"
